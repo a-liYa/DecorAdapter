0fb6f93961e5c178c903ca32b35ca53a39d3b932
a_liYa <liyaf@8531.cn>
2017-09-23 15:31:24 +0800

    解决加载更多后数据少量不回调loadMore()方法

--- app/src/main/java/com/aliya/adapter/simple/holder/FooterLoadMore.java
+++ app/src/main/java/com/aliya/adapter/simple/holder/FooterLoadMore.java
@@ -11,7 +11,7 @@ import com.aliya.adapter.simple.callback.LoadMoreListener;
 import com.aliya.adapter.simple.callback.LoadingCallBack;
 
 /**
- * 加载更多
+ * 加载更多，自定义示例
  *
  * @author a_liYa
  * @date 2017/8/24 18:11.
@@ -39,6 +39,16 @@ public class FooterLoadMore<M> extends PageItem implements LoadMore, View.OnClic
 
         this.loadMoreListener = loadMoreListener;
     }
+
+    // 防止加载少量数据，加载更多布局没出屏幕，不会回调 onViewAttachedToWindow
+    private Runnable mKeepRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (!isLoading && state != TYPE_ERROR && state != TYPE_NO_MORE) {
+                loadMore();
+            }
+        }
+    };
 
     @Override
     public void onClick(View view) {
@@ -77,6 +87,7 @@ public class FooterLoadMore<M> extends PageItem implements LoadMore, View.OnClic
 
     @Override
     public void onViewDetachedFromWindow(View view) {
+        itemView.removeCallbacks(mKeepRunnable);
     }
 
     @Override
@@ -96,6 +107,8 @@ public class FooterLoadMore<M> extends PageItem implements LoadMore, View.OnClic
 
     @Override
     public void onSuccess(M data) {
+        itemView.removeCallbacks(mKeepRunnable);
+        itemView.postDelayed(mKeepRunnable, 500);
         isLoading = false;
         if (loadMoreListener != null) {
             loadMoreListener.onLoadMoreSuccess(data, this);
